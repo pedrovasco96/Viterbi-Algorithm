ad20c7bffa56851a5423725ab0e0dceab605abd4
Pedro Vasco <pedro.vasco1996@gmail.com>
2019-02-05 00:18:41 +0000

    add user interface

--- viterbi.py
+++ viterbi.py
@@ -46,22 +46,36 @@ def vit(S, hmm, ep):
             ptr[j][i], = np.where(scorei == max(scorei))
             score[j][i] = ep[s[i]][j] + max(scorei)
 
-    res[len(S) - 1], = np.where(score[:, len(S) - 1] == max(score[:, len(S) - 1]))
+    res[len(S) - 1], = np.where(score[:, len(S) - 1] == max(score[:, len(S) - 1]))+1
 
     # cicle to determine the most probable sequence based in the traceback technique
     for i in range(len(S) - 1, 1, -1):
-        res[i - 1] = ptr[res[i]][i]
+        res[i - 1] = ptr[res[i]][i]+1
 
     print('Most probable sequence of states:')
     print(res)
 
+
 if __name__ == '__main__':
-    # state transitions matrix (markov model)
-    hmm = [[0.5, 0.5], [0.4, 0.6]]
-    # emition probabilities matrix with order (A-C-G-T) in rows
-    ep = [[0.2, 0.3], [0.3, 0.2], [0.3, 0.2], [0.2, 0.3]]
+
     # observed sequence
-    S = ["G", "G", "C", "A", "C", "T", "G", "A", "A"]
+    S = input("Insert observed sequence:")
+
+    states = int(input("Enter number of states:"))
+    hmm = np.zeros((states, states))
+    ep = np.zeros((4, states))
+
+    # state transitions matrix (markov model)
+    for i in range(states):
+        for j in range(states):
+            hmm[i][j] = float(input('Insert transition probability from state {} to state {} = '.format(i+1, j+1)))
+
+    # emition probabilities matrix with order (A-C-G-T) in rows
+    for i in range(states):
+        ep[0][i] = float(input('Insert emition probability of A by state {} = '.format(i + 1)))
+        ep[1][i] = float(input('Insert emition probability of C by state {} = '.format(i + 1)))
+        ep[2][i] = float(input('Insert emition probability of G by state {} = '.format(i + 1)))
+        ep[3][i] = float(input('Insert emition probability of T by state {} = '.format(i + 1)))
 
     vit(S, hmm, ep)
 
